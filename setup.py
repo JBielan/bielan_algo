e8b6016b99562bdd7e3436e7775f5b9e34d8a080
Jakub Bielan <31440616+jbielan@users.noreply.github.com>
2019-11-15 20:24:19 +0100

    bug-fix: import

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'bielan_algo',
   packages = ['bielan_algo'],
-  version = '0.11',
+  version = '0.12',
   license='MIT',
   description = 'Execution-time optimized solutions for various programming problems.',
   author_email = 'jkbielan@gmail.com',
   url = 'https://github.com/JBielan/bielan_algo',
-  download_url = 'https://github.com/JBielan/bielan_algo/archive/v_01.tar.gz',
+  download_url = 'https://github.com/JBielan/bielan_algo/archive/v_0.11.tar.gz',
   keywords = ['SOLUTIONS', 'ALGORITHMS', 'TIME-OPTIMIZED', 'PYTHON3', 'PYTHON'],
   install_requires=[
       ],
